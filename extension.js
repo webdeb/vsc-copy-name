ca4283269242ddec8040c23a7f5dc189c5775f39
Boris Kotov <bk@webdeb.de>
2018-02-23 16:02:52 +0100

    Make it work

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const copyPaste = require('copy-paste');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +15,13 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.copyName', function (explorerItem) {
         // The code you place here will be executed every time your command is executed
 
+        const baseName = path.basename(explorerItem.path);
+        copyPaste.copy(baseName);
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage(`Copied "${baseName}" to clipboard. Paste it somewhere.`);
     });
 
     context.subscriptions.push(disposable);
